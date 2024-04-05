32cedb3b914b8595f1b10cbe56b74b38fc2b4dd6
ethan-chu-shlawg <143014840+ethan-chu-shlawg@users.noreply.github.com>
2024-04-05 10:13:03 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,7 +1,23 @@
 import java.util.*;
 public class RandomStringChooser
 {
-  /* to be implemented in part (a) */
+  private String [] arr;
+  public RandomStringChooser(String [] words){
+    arr = words;
+  }
+  public String getNext(){
+    int randNum = (int)(Math.random()*arr.length);
+    String randWord = arr{randNum];
+    arr[randNum] = null;
+    int count = 0;
+    for(int i = 0; i < arr.length; i++){
+      if(arr[i] == null)
+        count ++;
+    }
+    if(count == arr.length)
+      return "NONE";
+    return randWord;
+  }
   
   //Heads up! 
   //You will get a very confusing error message until you have working code in part b as well
